dbf712aa4833d1503c18ac137b68bd44b014a0a3
Alan deLevie <adelevie@gmail.com>
2011-05-24 18:13:12 -0400

    removed useless methods

--- src/com/example/AsyncWeather/MainActivity.java
+++ src/com/example/AsyncWeather/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.AsyncWeather;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -46,68 +42,6 @@ public class MainActivity extends Activity {
 
     }
     
-    private String DownloadText(String URL)
-    {
-        int BUFFER_SIZE = 2000;
-        InputStream in = null;
-        try {
-            in = OpenHttpConnection(URL);
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-            return "";
-        }
-        
-        InputStreamReader isr = new InputStreamReader(in);
-        int charRead;
-        String str = "";
-        char[] inputBuffer = new char[BUFFER_SIZE];          
-        try {
-            while ((charRead = isr.read(inputBuffer))>0)
-            {                    
-                //—convert the chars to a String—
-                String readString = String.copyValueOf(inputBuffer, 0, charRead);
-                str += readString;
-                inputBuffer = new char[BUFFER_SIZE];
-            }
-            in.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return "";
-        }    
-        return str;        
-    }
-    
-    private InputStream OpenHttpConnection(String urlString) 
-    throws IOException
-    {
-        InputStream in = null;
-        int response = -1;
-               
-        URL url = new URL(urlString); 
-        URLConnection conn = url.openConnection();
-                 
-        if (!(conn instanceof HttpURLConnection))                     
-            throw new IOException("Not an HTTP connection");
-        
-        try{
-            HttpURLConnection httpConn = (HttpURLConnection) conn;
-            httpConn.setAllowUserInteraction(false);
-            httpConn.setInstanceFollowRedirects(true);
-            httpConn.setRequestMethod("GET");
-            httpConn.connect();
-            response = httpConn.getResponseCode();                 
-            if (response == HttpURLConnection.HTTP_OK) {
-                in = httpConn.getInputStream();                                 
-            }                     
-        }
-        catch (Exception ex)
-        {
-            throw new IOException("Error connecting");            
-        }
-        return in;     
-    }
     
     void examineJSONFile()
     {
@@ -116,16 +50,8 @@ public class MainActivity extends Activity {
             String x = "";
             
             String weatherUrl = "http://free.worldweatheronline.com/feed/weather.ashx?q=16803&format=json&num_of_days=5&key=586dac13bc212929112804";            
-           // String jsontext = DownloadText(weatherUrl)
-            
             String jsontext = getStringContent(weatherUrl);
-            
-            
-            
-            /*byte [] buffer = new byte[is.available()];
-            while (is.read(buffer) != -1);
-            String jsontext = new String(buffer);*/
-            
+
             JSONObject weather = new JSONObject(jsontext);
             JSONObject data = weather.getJSONObject("data");
             JSONArray currentCondition = new JSONArray(data.getString("current_condition"));
@@ -137,16 +63,6 @@ public class MainActivity extends Activity {
             x = currentTemperature + " degrees Farenheit\n\n";
             x += currentWeatherDescription;
 
-            /*
-            int i;
-            for (i=0;i<entries.length();i++)
-            {
-                JSONObject post = entries.getJSONObject(i);
-                x += "------------\n";
-                x += "Date:" + post.getString("created_at") + "\n";
-                x += "Post:" + post.getString("text") + "\n\n";
-            }
-            */
             tvData.setText(x);
         }
         catch (Exception je)
