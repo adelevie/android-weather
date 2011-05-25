97bc5ae4be6d9eab1f74886ba8ff32fcbf834051
Alan deLevie <adelevie@gmail.com>
2011-05-25 15:46:47 -0400

    removed extraneous files

--- gen/com/example/AsyncWeather/R.java
+++ gen/com/example/AsyncWeather/R.java
@@ -14,19 +14,15 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int ListView01=0x7f060001;
-        public static final int ScrollView01=0x7f060002;
-        public static final int btnJSON=0x7f060000;
+        public static final int ListView01=0x7f050001;
+        public static final int ScrollView01=0x7f050002;
+        public static final int btnJSON=0x7f050000;
     }
     public static final class layout {
-        public static final int list_item=0x7f030000;
-        public static final int main=0x7f030001;
-    }
-    public static final class raw {
-        public static final int jsonweather=0x7f040000;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
+        public static final int app_name=0x7f040001;
+        public static final int hello=0x7f040000;
     }
 }
